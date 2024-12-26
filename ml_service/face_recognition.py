00a8644a259edb4ba1501d6b351c00e260e7cb2c
Mingyu <qa7626@gmail.com>
2024-12-26 11:03:54 +0900

    Fix: ml_service/face_recognition

--- ml_service/face_recognition.py
+++ ml_service/face_recognition.py
@@ -2,30 +2,51 @@ import cv2
 import numpy as np
 from keras_facenet import FaceNet
 import mediapipe as mp
+import os
+import requests
+import logging
+import absl.logging
+
+# TensorFlow 경고 억제 설정
+mp.solutions.drawing_utils.OPTIONAL_LOGGING = False
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # WARNING 이상
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+absl.logging.set_verbosity(absl.logging.ERROR)
 
 
 class FaceRecognition:
-    detector = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.1)
-    embedder = FaceNet()
+    def __init__(self):
+        self.detector = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.3)
+        self.embedder = FaceNet()
 
-    def __init__(self, filepath):
-        self.img = cv2.imread(filepath)
-        self.detector_results = self.detector.process(cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB))
 
-    def extract_face(self):
+    @staticmethod
+    def image_url_downloader(url):
+        # 다운받을 이미지 URL
+        image = requests.get(url)
+        # 이미지 열기
+        img_array = np.asarray(bytearray(image.content), dtype=np.uint8)
+        img_cv = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+        return img_cv
+
+    def extract_face(self, img, is_path=False):
+        if is_path:
+            img = cv2.imread(img)
+        detector_results = self.detector.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
         box = list()
-
-        if self.detector_results.detections:
-            for i, detection in enumerate(self.detector_results.detections):
+        if detector_results.detections:
+            for i, detection in enumerate(detector_results.detections):
                 box.append(detection.location_data.relative_bounding_box)
 
-            xmin = max(int(box[0].xmin * self.img.shape[1]), 0)
-            ymin = max(int(box[0].ymin * self.img.shape[0]), 0)
-            width = int(box[0].width * self.img.shape[1])
-            height = int(box[0].height * self.img.shape[0])
+            xmin = max(int(box[0].xmin * img.shape[1]), 0)
+            ymin = max(int(box[0].ymin * img.shape[0]), 0)
+            width = int(box[0].width * img.shape[1])
+            height = int(box[0].height * img.shape[0])
 
-            face_img = self.img[ymin:ymin + height, xmin:xmin + width]
-            cv2.imwrite(f'face.jpg', face_img)
+            face_img = img[ymin:ymin + height, xmin:xmin + width]
+            cv2.imwrite(f'image/face.jpg', face_img)
 
 
         else:
@@ -52,4 +73,7 @@ class FaceRecognition:
         registered_face_embedding = np.array(registered_face_embedding)
         distance = np.linalg.norm(query_face_embedding - registered_face_embedding)  # L2 Norm
         print(distance)
+
+
+        return distance
 
